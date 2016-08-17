c57dea1ef249e8965b1767a399696fc413295488
Garrett Levine <garrlevine@gmail.com>
2016-08-16 22:26:24 -0400

    added the start of facebook auth

--- server/app.js
+++ server/app.js
@@ -6,8 +6,65 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var GitHubStrategy = require('passport-github').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
+
+// Configure GitHub Strategy
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: 'http://localhost:3000/auth/github/return',
+}, function(accessToken, refreshToken, profile, done) {
+  if (profile.emails[0]) {
+    User.findOneAndUpdate({
+      email: profilxe.emails[0].value,
+    },
+    {
+      name: profile.displayName || profile.username,
+      email: profile.emails[0].value,
+      photo: profile.photos[0].value,
+    },
+    {
+      upsert: true, 
+    }, done());
+  } else {
+    var noEmailError = new Error("Your email privacy settings prevent you from signing into hackeryou camp");
+
+    done(noEmailError, null);
+  }
+}));
+
+passprt.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_CLIENT_ID,
+  clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  callbackURL: 'http://localhost:3000/auth/facebook/return',
+}, function(accessToken, refreshToken, profile, done) {
+  if (profile.emails[0]) {
+    User.findOneAndUpdate({
+      email: profilxe.emails[0].value,
+    },
+    {
+      name: profile.displayName || profile.username,
+      email: profile.emails[0].value,
+      photo: profile.photos[0].value,
+    },
+    {
+      upsert: true, 
+    }, done());
+  } else {
+    var noEmailError = new Error("Your email privacy settings prevent you from signing into hackeryou camp");
+
+    done(noEmailError, null);}));
+
+passport.serializeUser(function(user, done){
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(userId, done) {
+  User.findById(userId, done(err, user));
+});
+
 
 // *************************************************
 //     A P P   I N I T I A L I Z A T I O N
