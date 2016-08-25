709ba766215295508a7b7b189e294dff4d782f52
Jenny Veens <jennyveens@gmail.com>
2016-08-24 21:55:19 -0400

    Updated top-nav styles - make matching to grid system - remove margin so no overhang on right side - update font to sans-serif variant

--- client/components/TopNav.jsx
+++ client/components/TopNav.jsx
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 
 import { Link } from 'react-router';
 
-import { Glyphicon } from 'react-bootstrap';
+import { Glyphicon, Grid, Row, Col } from 'react-bootstrap';
 
 import './TopNav.scss';
 
@@ -16,19 +16,25 @@ const defaultProps = {
 
 export default function TopNav(props) {
   return (
-    <nav className="top-nav">
-      <Link to="/" className="top-nav-item" activeClassName="active" onlyActiveOnIndex>
-        <Glyphicon glyph="calendar" />
-        <span>Events</span>
-      </Link>
-      <Link to="/leaderboard" className="top-nav-item" activeClassName="active">
-        <Glyphicon glyph="king" /><span>Scores</span>
-      </Link>
-      <Link to="/feed" className="top-nav-item" activeClassName="active">
-        <Glyphicon glyph="camera" />
-        <span>Feed</span>
-      </Link>
-    </nav>
+    <Grid>
+      <Row>
+        <Col xs={12}>
+          <nav className="top-nav">
+            <Link to="/" className="top-nav-item" activeClassName="active" onlyActiveOnIndex>
+              <Glyphicon glyph="calendar" />
+              <span>Events</span>
+            </Link>
+            <Link to="/leaderboard" className="top-nav-item" activeClassName="active">
+              <Glyphicon glyph="king" /><span>Scores</span>
+            </Link>
+            <Link to="/feed" className="top-nav-item" activeClassName="active">
+              <Glyphicon glyph="camera" />
+              <span>Feed</span>
+            </Link>
+          </nav>
+        </Col>
+      </Row>
+    </Grid>
   );
 }
 
