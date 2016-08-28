db33af624775c51fbc91b5322bb04b71ca8b38af
Matt Fairley <matt@crowdriff.com>
2016-08-28 14:52:33 -0400

    Photo component created

--- client/feed/Photo.jsx
+++ client/feed/Photo.jsx
@@ -1,16 +1,39 @@
 import React, { PropTypes } from 'react';
+import './Photo.scss';
+import linkify from 'linkify-instagram';
 
 const propTypes = {
-  // proptypes go here
+  photo: PropTypes.object.isRequired,
 };
 
 const defaultProps = {
   // Default props go here
 };
 
-export default function Photo(props) {
+
+export default function Photo({ photo }) {
   return (
-    <div></div>
+    <div "photo-wrapper">
+      <div className="img-container">
+        <img className="img" src={photo.thumbnail_gallery} alt={photo.text} />
+      </div>
+      <div className="img-details">
+        <div className="avatar-container">
+          <img
+            className="avatar"
+            src={asset.user_avatar_url}
+            alt={asset.user_screen_name}
+          />
+          <a href={`https://instagram.com/${asset.user_screen_name}`}>{asset.user_screen_name}</a>
+        </div>
+        <div 
+          className="caption" 
+          dangerouslySetInnerHTML={{
+            __html: linkify(asset.text)
+          }}
+        />
+      </div>
+    </div>
   );
 }
 
