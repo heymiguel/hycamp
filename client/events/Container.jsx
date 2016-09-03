9ca42892e8e12dea63c676af29ad84c9c687cfc1
Jenny Veens <jennyveens@gmail.com>
2016-09-03 16:41:53 -0400

    Sort events - events static JSON until I figure out db stuff

--- client/events/Container.jsx
+++ client/events/Container.jsx
@@ -20,11 +20,113 @@ export default class EventsContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // initial state
+    // initial state
+      events: [
+        {
+          name: 'Database Marketing',
+          location: '1295 Scoville Road',
+          date: '9/10/2016',
+          start_time: '5:50 PM',
+          duration: '1.1',
+        },
+        {
+          name: 'Tour Operators',
+          location: '6867 Crescent Oaks Lane',
+          date: '9/9/2016',
+          start_time: '12:18 PM',
+          duration: '2.6',
+        },
+        {
+          name: 'Wiring',
+          location: '47970 Schmedeman Lane',
+          date: '9/10/2016',
+          start_time: '1:00 PM',
+          duration: '2.9',
+        },
+        {
+          name: 'Ion Chromatography',
+          location: '705 Twin Pines Alley',
+          date: '9/10/2016',
+          start_time: '6:06 PM',
+          duration: '1.2',
+        },
+        {
+          name: 'Business Objects Data Integrator',
+          location: '934 Larry Point',
+          date: '9/9/2016',
+          start_time: '6:08 PM',
+          duration: '2.4',
+        },
+        {
+          name: 'Post Production',
+          location: '41617 Morningstar Point',
+          date: '9/10/2016',
+          start_time: '8:44 PM',
+          duration: '1.8',
+        },
+        {
+          name: 'VMware Certified Professional',
+          location: '32756 Porter Alley',
+          date: '9/9/2016',
+          start_time: '8:43 PM',
+          duration: '1.8',
+        },
+        {
+          name: 'Core FTP',
+          location: '19913 Manitowish Avenue',
+          date: '9/9/2016',
+          start_time: '7:40 PM',
+          duration: '1.5',
+        },
+      ],
+      days: [],
     };
   }
 
+  compare(a, b) {
+    return new Date(a.date).getTime() - new Date(b.date).getTime();
+  }
+
+  splitByDate(events, day) {
+    const dayEvents = [];
+    for (const event of events) {
+      if (event.date === day) {
+        dayEvents.push(event);
+      }
+    }
+    return dayEvents;
+  }
+
+  getDays(events) {
+    if (!events.length) {
+      return <div>No Events!</div>;
+    }
+    for (const event of events) {
+      if (this.state.days.indexOf(event.date) === -1) {
+        this.state.days.push(event.date);
+      }
+    }
+    // console.log(this.state.days);
+    return this.state.days;
+  }
+
+
+  renderDays(days) {
+    if (!days.length) {
+      return <div>No events!</div>;
+    }
+    return days.map(day => {
+      return (<EventDay
+        key={day}
+        day={day}
+        events={this.splitByDate(this.state.events, day)}
+        />);
+    });
+  }
+
   render() {
+    // sort events so they display in asc order
+    this.events = this.state.events.sort(this.compare);
     return (
       <Grid>
         <Row>
@@ -53,8 +155,7 @@ export default class EventsContainer extends React.Component {
 
         </div>
 
-        <EventDay />
-        <EventDay />
+        {this.renderDays(this.getDays(this.state.events))}
 
       </Grid>
     );
--- client/events/Event.jsx
+++ client/events/Event.jsx
@@ -3,30 +3,33 @@ import { Glyphicon, Grid, Row, Col, Clearfix } from 'react-bootstrap';
 
 const propTypes = {
   // proptypes go here
+  event: PropTypes.object.isRequired,
 };
 
 const defaultProps = {
   // Default props go here
 };
 
-export default function Event(props) {
-  return (
-    
-      <div className="event">
-      <Row>
-        <Col xs={2} className="text-center">
-          <Glyphicon glyph="star-empty" /> 12:00p
-        </Col>
-        <Col xs={5}>
-          Event Title
-        </Col>
-        <Col xs={5}>
-          Event Location
-        </Col>
-      </Row>
-      </div>
-      
-  );
+export default class Event extends React.Component {
+  render() {
+    const { event } = this.props;
+    return (
+        <div className="event">
+        <Row>
+          <Col xs={2} className="text-center">
+            <Glyphicon glyph="star-empty" />&nbsp;
+            {event.start_time}
+          </Col>
+          <Col xs={5}>
+            {event.name}
+          </Col>
+          <Col xs={5}>
+            {event.location}
+          </Col>
+        </Row>
+        </div>
+    );
+  }
 }
 
 Event.propTypes = propTypes;
