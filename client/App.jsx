1a5a72474ff8efa3b0af1f7a3ca718dd2bb85d66
Matt Fairley <matt@crowdriff.com>
2016-09-05 18:18:44 -0400

    Auth on front-end

--- client/App.jsx
+++ client/App.jsx
@@ -6,17 +6,54 @@ const propTypes = {
   children: PropTypes.node,
 };
 
-export default function App(props) {
-  return (
-    <div className="root">
-      <header>
-        <a href="http://localhost:3000/auth/login/github">github</a>
-        <a href="http://localhost:3000/auth/login/facebook">facebook</a>
-        <TopNav />
-      </header>
-      {props.children}
-    </div>
-  );
+function fetchUser() {
+  return new Promise((resolve, reject) => {
+    fetch('/api/user/getUser', { method: 'GET' })
+      .then(response => {
+        console.log(response.body);
+        response.json();
+      })
+      .then(data => {
+        console.log(data);
+        resolve(data);
+      })
+      .catch(err => {
+        console.log(err);
+        reject(err);
+      });
+  });
+}
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fetchingUser: true,
+      user: null,
+    };
+  }
+
+  componentDidMount() {
+    console.log('mounting');
+    fetchUser().then(response => {
+      this.setState({
+        user: response.data[0],
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="root">
+        <header>
+          <a href="http://localhost:3000/auth/login/github">github</a>
+          <a href="http://localhost:3000/auth/login/facebook">facebook</a>
+          <TopNav />
+        </header>
+        {this.props.children}
+      </div>
+    );
+  }
 }
 
 App.propTypes = propTypes;
